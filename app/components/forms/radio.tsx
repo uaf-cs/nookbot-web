f1d1c6354d390267015ee931051652e13cbb85a9
Katlyn Lorimer <katlyn@katlyn.dev>
2022-08-11 01:38:54 -0800

    feat: radio button dynamic options

--- app/components/forms/radio.tsx
+++ app/components/forms/radio.tsx
@@ -1,107 +1,41 @@
-import { useState } from 'react'
 import { RadioGroup } from '@headlessui/react'
+import { CheckIcon, MinusIcon } from '@heroicons/react/outline'
+import { classNames } from '~/utils'
 
-const plans = [
-  {
-    name: 'Startup',
-    ram: '12GB',
-    cpus: '6 CPUs',
-    disk: '160 GB SSD disk'
-  },
-  {
-    name: 'Business',
-    ram: '16GB',
-    cpus: '8 CPUs',
-    disk: '512 GB SSD disk'
-  },
-  {
-    name: 'Enterprise',
-    ram: '32GB',
-    cpus: '12 CPUs',
-    disk: '1024 GB SSD disk'
-  }
-]
-
-export default function Example () {
-  const [selected, setSelected] = useState(plans[0])
-
-  return (
-    <div className="w-full px-4 py-16">
-      <div className="mx-auto w-full max-w-md">
-        <RadioGroup value={selected} onChange={setSelected}>
-          <RadioGroup.Label className="sr-only">Server size</RadioGroup.Label>
-          <div className="space-y-2">
-            {plans.map((plan) => (
-              <RadioGroup.Option
-                key={plan.name}
-                value={plan}
-                className={({ active, checked }) =>
-                  `${
-                    active
-                      ? 'ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-uaf-636'
-                      : ''
-                  }
-                  ${
-                    checked ? 'bg-uaf-blue text-white' : 'bg-white'
-                  }
-                    relative flex cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none`
-                }
-              >
-                {({ active, checked }) => (
-                  <>
-                    <div className="flex w-full items-center justify-between">
-                      <div className="flex items-center">
-                        <div className="text-sm">
-                          <RadioGroup.Label
-                            as="p"
-                            className={`font-medium  ${
-                              checked ? 'text-white' : 'text-gray-900'
-                            }`}
-                          >
-                            {plan.name}
-                          </RadioGroup.Label>
-                          <RadioGroup.Description
-                            as="span"
-                            className={`inline ${
-                              checked ? 'text-sky-100' : 'text-gray-500'
-                            }`}
-                          >
-                            <span>
-                              {plan.ram}/{plan.cpus}
-                            </span>{' '}
-                            <span aria-hidden="true">&middot;</span>{' '}
-                            <span>{plan.disk}</span>
-                          </RadioGroup.Description>
-                        </div>
-                      </div>
-                      {checked && (
-                        <div className="shrink-0 text-white">
-                          <CheckIcon className="h-6 w-6" />
-                        </div>
-                      )}
-                    </div>
-                  </>
-                )}
-              </RadioGroup.Option>
-            ))}
-          </div>
-        </RadioGroup>
-      </div>
-    </div>
-  )
+interface RadioOption {
+  value: string
+  name: string
+}
+interface RadioProps {
+  label: string
+  name: string
+  options: RadioOption[]
+  selected: string | null
+  onChange: (value: string) => any
 }
 
-function CheckIcon (props: Record<string, any>) {
+export function Radio ({ label, name, options, selected, onChange }: RadioProps) {
   return (
-    <svg viewBox="0 0 24 24" fill="none" {...props}>
-      <circle cx={12} cy={12} r={12} fill="#fff" opacity="0.2" />
-      <path
-        d="M7 13l3 3 7-7"
-        stroke="#fff"
-        strokeWidth={1.5}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
+    <RadioGroup value={selected} name={name} onChange={onChange} className=' space-y-2'>
+      <RadioGroup.Label>{label}</RadioGroup.Label>
+      {options.map(opt => (
+        <RadioGroup.Option value={opt.value} key={opt.value}>
+          {({ active, checked }) => (
+            <div
+              className={classNames(
+                'p-2 flex justify-between border-l-4 border-solid cursor-pointer transition-all',
+                checked ? 'bg-uaf-636 border-uaf-blue shadow-lg' : 'bg-slate-200 border-slate-500 shadow hover:shadow-md hover:bg-slate-300',
+                active ? 'outline-2 outline-uaf-blue' : ''
+              )}
+            >
+              <span>{opt.name}</span>
+              {checked
+                ? (<CheckIcon className='w-6 h-6 text-uaf-blue' />)
+                : null}
+            </div>
+          )}
+        </RadioGroup.Option>
+      ))}
+    </RadioGroup>
   )
 }
