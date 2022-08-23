c2afea4e96abbb114b64a5c5f38099c37e4a6e1f
Katlyn Lorimer <katlyn@katlyn.dev>
2022-08-22 20:00:40 -0800

    feat: basic academic status validation

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,7 +1,7 @@
 import { AcademicStatus, DiscordUser, User } from '@prisma/client'
-import type { LoaderFunction } from '@remix-run/node'
+import type { ActionFunction, ErrorBoundaryComponent, LoaderFunction } from '@remix-run/node'
 import { json, redirect } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import { Form, useLoaderData } from '@remix-run/react'
 import { useState } from 'react'
 import { Radio } from '~/components/forms/radio'
 import { Callout, CalloutTypes } from '~/components/layout/callout'
@@ -13,6 +13,18 @@ import { prisma } from '~/services/prisma.server'
 interface LoaderData {
   user: User
   discord: DiscordUser
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData()
+
+  const academicStatus = await formData.get('academic-status')
+
+  if (typeof academicStatus !== 'string' || !Object.keys(AcademicStatus).includes(academicStatus)) {
+    throw new TypeError(`academic-status should be one of: ${Object.keys(AcademicStatus).toString()}`)
+  }
+
+  return null
 }
 
 export const loader: LoaderFunction = async ({ request }) => {
@@ -50,13 +62,27 @@ export default function Dashboard () {
   return (
     <CenterContent>
       <h1 className='text text-4xl'>User Settings</h1>
-      <Radio
-        label='Academic Status'
-        name='academic-status'
-        options={academicStatusOptions}
-        selected={selection}
-        onChange={setSelection}
-      />
+      <Form method='post'>
+        <Radio
+          label='Academic Status'
+          name='academic-status'
+          options={academicStatusOptions}
+          selected={selection}
+          onChange={setSelection}
+        />
+        <div className="flex mt-6 justify-end">
+          <button className='button success' type="submit">Save</button>
+        </div>
+      </Form>
     </CenterContent>
+  )
+}
+
+export function ErrorBoundary ({ error }: { error: Error }) {
+  return (
+    <>
+      <h1>Unexpected Error: {error.name}</h1>
+      <p>{error.message}</p>
+    </>
   )
 }
